07b37b6fd082b17b77058818746411ee2d015e93
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-06-15 05:13:02 +0500

    App Created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <h2 className="header">
+            The Expense Tracker
+        </h2>
+    )
+}
+export default Header
